4a1f4fb2016b71def705181e3df0ece3a41a234d
Zaka531 <amrizakaria999@proton.me>
2024-04-07 21:42:31 +0700

    chime: AfterLife Bringup
    Signed-off-by: Zaka531 <amrizakaria999@proton.me>

--- custom.mk
+++ custom.mk
@@ -4,18 +4,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common AfterLife stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Bootanimation
-USE_LEGACY_BOOTANIMATION := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
-# Pixel customization
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_CALL_RECORDING := true
+# Blurs
+TARGET_SUPPORTS_BLUR := false
 
-# Screen Resolution
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+
+# Gapps
+AFTERLIFE_GAPPS := true
+
+# Maintainer
+AFTERLIFE_MAINTAINER := AmriZakaria
+
+# Touch Gestures
+TARGET_SUPPORTS_TOUCHGESTURES := true
